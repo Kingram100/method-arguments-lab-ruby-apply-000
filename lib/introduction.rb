0aef6e178a582dae237f7a8ec9876476b43031f1
Karl Ingram <kingram100@yahoo.com>
2016-10-06 14:09:58 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+def introduction(input)
+  puts "Hi, my name is #{input}."
+end
+
+def introduction_with_language(input1,input2)
+  puts "Hi, my name is #{input1} and I am learning to program in #{input2}."
+
+end
